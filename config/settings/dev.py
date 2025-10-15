9b55ca7760883445364fac549a1da24e42486488
LuSiEDa <wltlrtmdgus@naver.com>
2025-10-15 19:14:53 +0900

    renew

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -6,12 +6,11 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("MYSQL_DATABASE", "viral_db"),
-        "USER": os.getenv("MYSQL_USER", "root"),
-        "PASSWORD": os.getenv("MYSQL_PASSWORD", ""),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", "viral_db"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "password1234"),
         "HOST": os.getenv("DB_HOST", "db"),  # docker-compose의 서비스명
-        "PORT": os.getenv("DB_PORT", "3306"),
-        "OPTIONS": {"init_command": "SET sql_mode='STRICT_ALL_TABLES'"},
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
