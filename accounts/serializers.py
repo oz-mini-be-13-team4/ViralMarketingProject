beaf244a01b28d730e405f98a29dc3609730de56
grrr127 <qrrr127@gmail.com>
2025-10-18 18:26:34 +0900

    feat(accounts):계좌 CRUD API 추가

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -23,6 +23,22 @@ class UserSignUpSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+from rest_framework import serializers
+from accounts.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password', 'nickname', 'name', 'phone_number')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # 비밀번호 해시 처리
+        user.save()
+        return user
 
 class AccountSerializer(serializers.ModelSerializer):
     user_email = serializers.EmailField(source="user.email", read_only=True)
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 
 from django.contrib import admin
+from accounts.views import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import path, include
 
 from accounts.views import UserActivateView, UserSignUpView
@@ -8,6 +10,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("signup/", UserSignUpView.as_view(), name="signup"),
     path("activate/<str:uid64>/<str:token>/", UserActivateView.as_view(), name="activate"),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/", include("accounts.urls")),
 
     # 테스트때문에 넣은거
