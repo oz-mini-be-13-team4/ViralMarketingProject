75b7e9dd9dab5888b78f542910536119522953ea
kickcik <4778009@gmail.com>
2025-10-18 21:09:53 +0900

    import 변경

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,16 @@
-
 from django.contrib import admin
-from accounts.views import CustomTokenObtainPairView
-from rest_framework_simplejwt.views import TokenRefreshView
-from django.urls import path, include
-from django.urls import path
-from accounts.views import CustomTokenObtainPairView
+from django.urls import include, path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from accounts.views import UserActivateView, UserSignUpView
+from accounts.views import CustomTokenObtainPairView, UserActivateView, UserSignUpView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("signup/", UserSignUpView.as_view(), name="signup"),
     path("activate/<str:uid64>/<str:token>/", UserActivateView.as_view(), name="activate"),
-    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("login/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("accounts.urls")),
-
     # 테스트때문에 넣은거
     path("api-auth/", include("rest_framework.urls")),
 ]
