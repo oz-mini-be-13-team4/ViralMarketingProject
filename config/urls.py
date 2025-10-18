a5051b010e89fd6087fdb133fd36935b5238a441
kickcik <4778009@gmail.com>
2025-10-18 21:15:50 +0900

    urls 추가

--- config/urls.py
+++ config/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("activate/<str:uid64>/<str:token>/", UserActivateView.as_view(), name="activate"),
     path("login/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("accounts/<str:uid64>/<str:token>/", UserActivateView.as_view(), name="activate"),
     path("api/", include("accounts.urls")),
     # 테스트때문에 넣은거
     path("api-auth/", include("rest_framework.urls")),
