514280ad9810c7f0c2bbecdcd7e67c68a0380228
kickcik <4778009@gmail.com>
2025-10-18 16:18:22 +0900

    django-filter 설치 및 REST_FRAMEWORK 설정

--- config/settings/base.py
+++ config/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "accounts",
     "rest_framework",
+    "django_filters",
 ]
 AUTH_USER_MODEL = "accounts.User"
 
@@ -122,3 +123,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+}
