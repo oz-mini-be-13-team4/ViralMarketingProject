42e4bfe12b1179cb31de6207486347ca7426dcfc
daebagi <daeburm0115@naver.com>
2025-10-18 22:20:33 +0900

    처음으로 수정

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -10,7 +10,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME", "viral_db"),
         "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "qkrl0115**"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "password1234"),
         "HOST": os.getenv("DB_HOST", "127.0.0.1"),  # docker-compose의 서비스명
         "PORT": os.getenv("DB_PORT", "5432"),
     }
