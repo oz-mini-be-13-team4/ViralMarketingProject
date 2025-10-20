24dfd4ad8363544610e13c2c2d1e3d757ea8e7c5
kickcik <4778009@gmail.com>
2025-10-20 14:53:06 +0900

    db 설정 변경 (psql)

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,37 +1,35 @@
 import os
-from config.settings.base import * #noqa
 
-from config.settings.base import *  # noqa
+from config.settings.base import *  # noqa  # noqa
 
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("MYSQL_DATABASE"),
-        "USER": os.getenv("MYSQL_USER"),
-        "PASSWORD": os.getenv("MYSQL_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "db"),
-        "PORT": os.getenv("DB_PORT", "3306"),
-        "OPTIONS": {"init_command": "SET sql_mode='STRICT_ALL_TABLES'"},
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", "viral_db"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "password1234"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),  # docker-compose의 서비스명
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
-# 이메일 발송에 사용할 백엔드
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-# SMTP 서버 주소 및 포트
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True  # TLS 보안 연결 사용
-
-# Gmail 계정 정보 (앱 비밀번호 사용!)
-EMAIL_HOST_USER = "your_gmail_account@gmail.com"
-EMAIL_HOST_PASSWORD = "your_app_password"
-
-# 발신자 주소 (표시되는 이름)
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# # 이메일 발송에 사용할 백엔드
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#
+# # SMTP 서버 주소 및 포트
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True  # TLS 보안 연결 사용
+#
+# # Gmail 계정 정보 (앱 비밀번호 사용!)
+# EMAIL_HOST_USER = "your_gmail_account@gmail.com"
+# EMAIL_HOST_PASSWORD = "your_app_password"
+#
+# # 발신자 주소 (표시되는 이름)
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Gmail은 보안 강화로 인해 일반 비밀번호로는 SMTP 접근이 막혀 있음.
 # “앱 비밀번호”를 발급받아서 EMAIL_HOST_PASSWORD에 넣어야 함.
